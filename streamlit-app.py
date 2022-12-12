302ea04ce3b843c065efe9a5ad28d7e5c1702ca3
spikspiks <dasabhijeet.1990@gmail.com>
2022-12-12 08:02:38 +0530

    map added

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pickle
+import pandas as pd
 
 if __name__=='__main__':
     model = pickle.load(open('cal_housing_model.pkl','rb'))
@@ -24,20 +25,20 @@ if __name__=='__main__':
                 - Longitude     block group longitude 
                 """)
     with st.form("Inputs"):
-        medinc = st.number_input("Median Income in Block Group (in $10,000)")
+        medinc = st.number_input("Median Income in Block Group (in $10,000)",value=4.0,step=0.01)
         
-        houseage = st.number_input("Median House Age in Block Group",step=1)
+        houseage = st.number_input("Median House Age in Block Group",value=25,step=1)
         
-        averooms = st.number_input("Average Number of Rooms per Household",step=1)
+        averooms = st.number_input("Average Number of Rooms per Household",value=6,step=1)
         
-        avebedrms = st.number_input("Average Number of Bedrooms per Household",step=1)
+        avebedrms = st.number_input("Average Number of Bedrooms per Household",value=2,step=1)
         
-        population = st.number_input("Block Group Population",step=1)
+        population = st.number_input("Block Group Population",value=1500,step=10)
         
-        aveoccup = st.number_input("Average Number of Household Members",step=1)
+        aveoccup = st.number_input("Average Number of Household Members",value=4,step=1)
         
-        latitude = st.slider("Latitude (limited to California)", min_value=32.54,max_value=41.95,step=0.01,value=(32.54+41.95)/2)
-        longitude = st.slider("Longitude (limited to California)", min_value=-124.35,max_value=-114.31,step=0.01,value=-(124.35+114.31)/2)
+        latitude = st.slider("Latitude (limited to California)", min_value=32.54,max_value=41.95,step=0.01,value=37.76)
+        longitude = st.slider("Longitude (limited to California)", min_value=-124.35,max_value=-114.31,step=0.01,value=-121.51)
         
         submit_form = st.form_submit_button("Submit")
     
@@ -47,6 +48,10 @@ if __name__=='__main__':
         
         predicted_price=int(model.predict(scaled_vector)[0]*100000)
         st.write("The predicted median house price is **$ {:,}**".format(predicted_price))
+        
+        location = pd.DataFrame([[latitude,longitude]],columns=['lat','lon'])
+        st.map(location)
+        
         st.markdown("-----")
-        st.write("This model was created by training a CatBoostClassifier on the famous California Housing Price dataset")
-        st.write("You can look at the source code [here](https://github.com/spikspiks/cali_housepricing)")
+        st.write("This model was created by training a CatBoostClassifier on the California Housing Price dataset.")
+        st.write("Check out the source code [here](https://github.com/spikspiks/cali_housepricing)")
